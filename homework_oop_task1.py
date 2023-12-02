e73164ca1d84d55b5de59c5c4f7872c45184211c
Vadim Bondarenko <vdm88@mail.ru>
2023-12-02 21:21:08 +0700

    added task1

--- homework_oop_task1.py
+++ homework_oop_task1.py
@@ -14,6 +14,14 @@ class Mentor:
         self.courses_attached = []
 
 
+class Lecturer(Mentor):
+    pass
+
+
+class Reviewer(Mentor):
+    pass
+
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
